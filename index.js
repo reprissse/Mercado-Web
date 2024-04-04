ada4fd0563389b7f875aa81ee2d3354b556c7688
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-04-04 18:56:05 -0300

    :sparkles: sitio mercado web

---
+++ index.js
@@ -0,0 +1,29 @@
+//importar dependecias
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars')
+const PORT = 3003;
+
+//configuración del motor de plantilla
+app.set('view engine', 'hbs')
+app.engine('hbs', exphbs.engine({
+    extname:'hbs'
+}));
+app.set('views', __dirname + '/views');
+
+
+//rutas statics
+app.use(express.static('assets'))
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist'))
+
+
+//Rutas raiz
+app.get('/', (req, res) => {
+    // Renderizar la vista 'home' con los productos
+    res.render('home', {
+        products: ['banana', 'cebollas', 'pimenton', 'papas', 'lechuga', 'tomate']
+    })
+});
+
+//iniciar el servidor
+app.listen(PORT, () => console.log(`El servidor se ha llamado en el port http://localhost:${PORT}`))
